075b16777ca53500f77a8c6fbb43f32b754f7550
chidelma <samuel@clickspace.com>
2024-07-15 12:08:13 -0400

    feat: use of references

---
+++ types/general.d.ts
@@ -0,0 +1,14 @@
+type _uuid = `${string}-${string}-${string}-${string}-${string}`
+
+interface _storeCursor<T> {
+    [Symbol.asyncIterator](): AsyncGenerator<Map<_uuid, T> | Map<_uuid, Partial<T>> | _uuid, void, unknown>
+    next(limit?: number): Promise<Map<_uuid, T> | Map<_uuid, Partial<T>> | _uuid[]>
+    onDelete(): AsyncGenerator<_uuid, void, unknown>
+}
+
+interface _treeItem<T> {
+    field: keyof T
+    type?: 'string' | 'number' | 'boolean' | 'string | null' | 'number | null',
+    default?: string | number | boolean | null
+    children?: _treeItem<T>[]
+}
